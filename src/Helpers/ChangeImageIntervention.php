f59f93d6d654cc59b2555a7a32cf6ea53744ddc9
Paharok <soltanovka@gmail.com>
2022-09-30 21:47:57 +0300

    fix errors

--- src/Helpers/ChangeImageIntervention.php
+++ src/Helpers/ChangeImageIntervention.php
@@ -8,96 +8,96 @@ use Intervention\Image\Facades\Image AS Image;
 class ChangeImageIntervention implements ChangeImage{
 
 
-	public static function changeImage($filePath,$width=0,$height=0,$crop='fit'){
+    public static function changeImage($filePath,$width=0,$height=0,$crop='fit'){
 
         if(mb_substr($filePath,0,1) != '/'){
             $filePath = '/' . $filePath;
         }
 
-		if(!$filePath || !file_exists(public_path() . $filePath)){
+        if(!$filePath || !file_exists(public_path() . $filePath)){
 
-			$filePath = 'vendor/laravel-files/files/no-img.png';
-		}
-		$image_size = getimagesize(public_path() . $filePath);
-
-
-		if($width==0 && $height==0){
-			$width = $image_size[0];
-			$height = $image_size[1];
-		}
-
-		if($width==0){
-			$width = round(($image_size[0] / $image_size[1]) * $height);
-		}
-
-		if($height==0){
-			$height = round($width / ($image_size[0] / $image_size[1]));
-		}
+            $filePath = 'vendor/laravel-files/files/no-img.png';
+        }
+        $image_size = getimagesize(public_path() . $filePath);
 
 
-		$path_parts = pathinfo($filePath);
+        if($width==0 && $height==0){
+            $width = $image_size[0];
+            $height = $image_size[1];
+        }
 
-		$mainMime = mime_content_type(public_path() . $filePath);
-		$cache_dir = $path_parts['dirname'] . '/__thumbnails__/';
-		$newFileName = $path_parts['filename'] . $width . '_'.$height . $crop . '.'. $path_parts['extension'];
-		$newFileNameWebp = $path_parts['filename'] . $width . '_'.$height . $crop . '.webp';
+        if($width==0){
+            $width = round(($image_size[0] / $image_size[1]) * $height);
+        }
 
-		if(!file_exists(public_path() . '/' . $cache_dir . $newFileName)){
+        if($height==0){
+            $height = round($width / ($image_size[0] / $image_size[1]));
+        }
 
 
+        $path_parts = pathinfo($filePath);
 
-			$image = Image::make(public_path() . $filePath);
+        $mainMime = mime_content_type(public_path() . $filePath);
+        $cache_dir = $path_parts['dirname'] . '/__thumbnails__/';
+        $newFileName = $path_parts['filename'] . $width . '_'.$height . $crop . '.'. $path_parts['extension'];
+        $newFileNameWebp = $path_parts['filename'] . $width . '_'.$height . $crop . '.webp';
+
+        if(!file_exists(public_path() . '/' . $cache_dir . $newFileName)){
 
 
 
-
-			if(!file_exists(public_path() . '/' . $cache_dir)){
-				mkdir(public_path() . '/' . $cache_dir,0775,true);
-			}
-
-			if($crop=='fit'){
-				$image->fit($width, $height, function ($constraint) {
-					$constraint->aspectRatio();
-					$constraint->upsize();
-				});
-			}elseif($crop=='resize'){
-				$image->resize($width, $height, function ($constraint) {
-					$constraint->aspectRatio();
-					$constraint->upsize();
-				});
-			}elseif($crop=='resizebg'){
-				$canvas = Image::canvas($width, $height, '#ffffff');
-
-				$image->resize($width, $height, function ($constraint) {
-					$constraint->aspectRatio();
-					$constraint->upsize();
-				});
-
-				$image = $canvas->insert($image,'center');
-
-			}
+            $image = Image::make(public_path() . $filePath);
 
 
 
-			$image->save(public_path() . '/' . $cache_dir . $newFileName);
-			$image->save(public_path() . '/' . $cache_dir . $newFileNameWebp);
-		}
 
-	$cache_dir = str_replace(' ','%20',$cache_dir);
+            if(!file_exists(public_path() . $cache_dir)){
+                mkdir(public_path() . $cache_dir,0775,true);
+            }
 
-		return [
-				'main_uri'=> '/'. $cache_dir . str_replace(' ','%20',$newFileName),
-				'original_uri'=> str_replace(' ','%20',$filePath),
-				'sources'=>[
-								'image/webp'=>'/'. $cache_dir . str_replace(' ','%20',$newFileNameWebp),
-								$mainMime=>'/'. $cache_dir . str_replace(' ','%20',$newFileName)
+            if($crop=='fit'){
+                $image->fit($width, $height, function ($constraint) {
+                    $constraint->aspectRatio();
+                    $constraint->upsize();
+                });
+            }elseif($crop=='resize'){
+                $image->resize($width, $height, function ($constraint) {
+                    $constraint->aspectRatio();
+                    $constraint->upsize();
+                });
+            }elseif($crop=='resizebg'){
+                $canvas = Image::canvas($width, $height, '#ffffff');
 
-							]
+                $image->resize($width, $height, function ($constraint) {
+                    $constraint->aspectRatio();
+                    $constraint->upsize();
+                });
 
-				];
+                $image = $canvas->insert($image,'center');
 
-	}
+            }
+
+
+
+            $image->save(public_path() . $cache_dir . $newFileName);
+            $image->save(public_path() . $cache_dir . $newFileNameWebp);
+        }
+
+        $cache_dir = str_replace(' ','%20',$cache_dir);
+
+        return [
+            'main_uri'=> $cache_dir . str_replace(' ','%20',$newFileName),
+            'original_uri'=> str_replace(' ','%20',$filePath),
+            'sources'=>[
+                'image/webp'=> $cache_dir . str_replace(' ','%20',$newFileNameWebp),
+                $mainMime=> $cache_dir . str_replace(' ','%20',$newFileName)
+
+            ]
+
+        ];
+
+    }
 
 }
 
-  ?>
+?>
